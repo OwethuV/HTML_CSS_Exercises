f4b7b91af882c581865956395250a154b601f3b4
OwethuV <outisvalantiya@gmail.com>
2025-05-30 10:21:30 +0200

    Created array.js

---
+++ JS-Exercise-3/array.js
@@ -0,0 +1,13 @@
+// TODO: Create an empty array called colors
+let colors = [];
+// TODO: Add three colors to the array using push()
+colors.push('red', 'green', 'blue');
+// TODO: Create another array called numbers with five numbers
+let numbers = [17, 4, 21, 32, 224];
+// TODO: Remove the last color from the colors array
+colors.pop();
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift('yellow');
+// TODO: Print the length of both arrays
+console.log('Length of colors array:', colors.length);
+console.log('Length of numbers array:', numbers.length);
