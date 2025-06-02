d3947528715bd582277f5b37f4b3a284adcdc86b
OwethuV <outisvalantiya@gmail.com>
2025-06-02 12:35:22 +0200

    Created functions.js

---
+++ JS-Exercise-6/functions.js
@@ -0,0 +1,20 @@
+// TODO: Create a function called greet that takes a name parameter
+// TODO: Make the greet function return "Hello, [name]!"
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// TODO: Create a function called add that takes two parameters
+// TODO: Make the add function return the sum of both parameters
+
+function add(a, b) {
+    return a + b;
+}
+
+// TODO: Call both functions and store their results in variables
+const greeting = greet("Alice");
+const sum = add(5, 10);
+
+// TODO: Print the results to the console
+console.log(greeting);
+console.log(sum);
